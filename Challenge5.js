4a0e7973eb86eb879ffd3edd58903c3ac8c9072b
Nicholas <71210684@students.ukdw.ac.id>
2023-09-17 14:28:12 +0700

    Modified :Lingkaran & Segitiga

--- Challenge5.js
+++ Challenge5.js
@@ -1,83 +1,121 @@
-// Persegi
-class Persegi{
-    constructor(sisi){
-        this.sisi = sisi
-    }
-    
-    kelilingPersegi(){
+// Persegi (Agustinus)
+class Persegi {
+  constructor(sisi) {
+    this.sisi = sisi;
+  }
 
-        return 4 * this.sisi;
+  kelilingPersegi() {
+    return 4 * this.sisi;
+  }
 
-    }
+  luasPersegi() {
+    const result = this.sisi * this.sisi;
 
-    luasPersegi(){
-        const result =  this.sisi * this.sisi;
-
-        return result;
-    }
-
+    return result;
+  }
 }
 
-
-const newPersegi = new Persegi(5)
+const newPersegi = new Persegi(5);
 
 console.log("PERSEGI :");
-console.log("keliling persegi", newPersegi.kelilingPersegi())
-console.log( "Luas persegi  ",newPersegi.luasPersegi())
+console.log("keliling persegi", newPersegi.kelilingPersegi());
+console.log("Luas persegi  ", newPersegi.luasPersegi());
 
-// Persegi Panjang
+// Persegi Panjang (Agus)
 class PersegiPanjang {
-    constructor(panjang, lebar) {
-      this.panjang = panjang;
-      this.lebar = lebar;
-    }
-  
-    keliling() {
-      return 2 * (this.panjang + this.lebar);
-    }
-  
-    luas() {
-      return this.panjang * this.lebar;
-    }
+  constructor(panjang, lebar) {
+    this.panjang = panjang;
+    this.lebar = lebar;
   }
-  
-  const persegiPanjang = new PersegiPanjang(5, 10);
-  
-  console.log("\nPERSEGI PANJANG:");
-  console.log("Keliling:", persegiPanjang.keliling());
-  console.log("Luas:", persegiPanjang.luas());
+
+  keliling() {
+    return 2 * (this.panjang + this.lebar);
+  }
+
+  luas() {
+    return this.panjang * this.lebar;
+  }
+}
+
+const persegiPanjang = new PersegiPanjang(5, 10);
+
+console.log("\nPERSEGI PANJANG:");
+console.log("Keliling:", persegiPanjang.keliling());
+console.log("Luas:", persegiPanjang.luas());
+
+// Lingkaran (Aditya)
+class Lingkaran {
+  constructor(jariJari) {
+    this.jariJari = jariJari;
+  }
+
+  hitungLuas() {
+    return (Math.PI * this.jariJari ** 2).toFixed(3);
+  }
+
+  hitungKeliling() {
+    return (2 * Math.PI * this.jariJari).toFixed(3);
+  }
+}
+
+const lingkaran = new Lingkaran(5);
+
+console.log("\nLINGKARAN :");
+console.log(`Luas lingkaran: ${lingkaran.hitungLuas()}`);
+console.log(`Keliling Lingkaran: ${lingkaran.hitungKeliling()}`);
+
+// Segitiga (Fajar)
+class Segitiga {
+    constructor(alas, tinggi, sisi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+        this.sisi = sisi;
+    }
+
+    luasSegitiga() {
+        return (1/2) * this.alas * this.tinggi;
+    }
+
+    kelilingSegitiga() {
+        return this.alas + this.tinggi + this.sisi;
+    }
+}
+
+const segitiga = new Segitiga(8, 12, 13);
+
+console.log("\nSEGITIGA :");
+console.log("Luas Segitiga:", segitiga.luasSegitiga());
+console.log("Keliling Segitiga:", segitiga.kelilingSegitiga());
 
 
-// Trapesium
+// Trapesium (Nicholas)
 class Trapesium {
-    constructor(a, b, c, d, h) {
-        this.sisiA = a;
-        this.sisiB = b;
-        this.sisiC = c;
-        this.sisiD = d;
-        this.tinggi = h;
-    }
+  constructor(a, b, c, d, h) {
+    this.sisiA = a;
+    this.sisiB = b;
+    this.sisiC = c;
+    this.sisiD = d;
+    this.tinggi = h;
+  }
 
-    // fungsi keliling
-    kelilingTrapesium() {
-        return this.sisiA + this.sisiB + this.sisiC + this.sisiD;
-    }
+  // fungsi keliling
+  kelilingTrapesium() {
+    return this.sisiA + this.sisiB + this.sisiC + this.sisiD;
+  }
 
-    // fungsi luas
-    luasTrapesium() {
-        return (1/2) * (this.sisiA + this.sisiB) * this.tinggi;
-    }
+  // fungsi luas
+  luasTrapesium() {
+    return (1 / 2) * (this.sisiA + this.sisiB) * this.tinggi;
+  }
 }
 
 // Membuat objek trapesium
 const trapesiumContoh = new Trapesium(5, 7, 4, 6, 3);
 
-
-
 // Menggunakan metode untuk menghitung keliling dan luas
 const keliling = trapesiumContoh.kelilingTrapesium();
 const luas = trapesiumContoh.luasTrapesium();
 
-console.log("\nTrapesium :");
+console.log("\nTRAPESIUM :");
 console.log("Keliling trapesium:", keliling);
 console.log("Luas trapesium:", luas);
